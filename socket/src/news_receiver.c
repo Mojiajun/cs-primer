1fe7aeacacef6436bee1eee6d2f0c2cbffc9717a
WuYong <wuyongchn@126.com>
2019-06-05 19:14:08 +0800

    udp multicasting

--- socket/src/news_receiver.c
+++ socket/src/news_receiver.c
@@ -1,3 +1,8 @@
+/*
+ * 实现多播Sender和Receiver
+ * Receiver：接受×××组的新闻信息
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- socket/src/news_sender.c
+++ socket/src/news_sender.c
@@ -1,3 +1,8 @@
+/*
+ * 实现多播Sender和Receiver
+ * Sender：向×××组多播播文件中保存的新闻信息
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
